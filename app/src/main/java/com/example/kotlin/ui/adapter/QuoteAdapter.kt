7f85a9e42b6c11d447e7c201a8382706602d6945
VD <valicsekdavid@VDs-MacBook-Pro.local>
2019-11-29 14:31:44 +0100

    Create custom filtering for Adapter

--- app/src/main/java/com/example/kotlin/ui/adapter/QuoteAdapter.kt
+++ app/src/main/java/com/example/kotlin/ui/adapter/QuoteAdapter.kt
@@ -8,6 +8,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.Filter
+import android.widget.Filterable
 import com.example.kotlin.data.model.Quote
 
 
@@ -18,6 +20,7 @@ class QuoteAdapter(
 ) : ArrayAdapter<Quote>(context, textViewResourceId, quoteList) {
 
     private var quoteList: List<Quote> = quoteList
+    private var filteredQuoteList: List<Quote> = quoteList
 
     /*
     init {
@@ -25,16 +28,50 @@ class QuoteAdapter(
     }
     */
 
+
+    override fun getCount(): Int {
+        return filteredQuoteList.size
+    }
+
+    override fun getItem(position: Int): Quote? {
+        return filteredQuoteList[position]
+    }
+
     @SuppressLint("ViewHolder")
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
 
         val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
         val view = inflater.inflate(R.layout.simple_list_item_1, null)
 
-        view.findViewById<TextView>(R.id.text1).text = quoteList[position].author + " : " + quoteList[position].quoteText
+        view.findViewById<TextView>(R.id.text1).text = filteredQuoteList[position].author + " : " + filteredQuoteList[position].quoteText
 
 
         // return super.getView(position, convertView, parent)
         return view
     }
+
+    override fun getFilter(): Filter {
+        return object : Filter() {
+
+            override fun publishResults(charSequence: CharSequence?, filterResults: FilterResults) {
+                filteredQuoteList = filterResults.values as List<Quote>
+                notifyDataSetChanged()
+            }
+
+            override fun performFiltering(charSequence: CharSequence?): FilterResults {
+                val queryString = charSequence?.toString()?.toLowerCase()
+
+                val filterResults = FilterResults()
+                filterResults.values = if (queryString==null || queryString.isEmpty())
+                    quoteList
+                else
+                    quoteList.filter {
+                        it.author.toLowerCase().contains(queryString) ||
+                                it.quoteText.toLowerCase().contains(queryString)
+                    }
+                return filterResults
+            }
+        }
+    }
+        // return super.getFilter()
 }
